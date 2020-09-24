d3bea751e02b15f0892e6b95036830fb56ae3d35
meziantou <meziantou@live.fr>
2020-09-24 14:44:27 -0400

    Split feeds for issues and PR + generate OPML

--- src/IssuesToRss/Program.cs
+++ src/IssuesToRss/Program.cs
@@ -2,95 +2,175 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.ServiceModel.Syndication;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using Meziantou.Framework;
 using Octokit;
 using Octokit.Internal;
 
 namespace IssuesToRss
 {
+    internal static class Configuration
+    {
+        public const string GitHubRepositoryUrl = "https://github.com/meziantou/IssuesToRss/";
+        public const string RootUrl = "https://meziantou.github.io/IssuesToRss/";
+
+        public static IReadOnlyCollection<string> Repositories { get; } = new[]
+        {
+            "dotnet/aspnetcore",
+            "dotnet/csharplang",
+            "dotnet/efcore",
+            "dotnet/format",
+            "dotnet/machinelearning",
+            "dotnet/orleans",
+            "dotnet/roslyn",
+            "dotnet/roslyn-analyzers",
+            "dotnet/runtime",
+            "dotnet/SqlClient",
+            "dotnet/windowsdesktop",
+            "dotnet/winforms",
+            "dotnet/wpf",
+        };
+
+        public static IReadOnlySet<string> ExcludedUsers { get; } = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "dotnet-bot",
+            "dotnet-maestro-bot",
+            "dotnet-maestro[bot]",
+        };
+
+        public static IReadOnlySet<string> ExcludedLabels { get; } = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "Type: Dependency Update :arrow_up_small:",
+        };
+    }
+
     internal static class Program
     {
         static async Task Main(string[] args)
         {
             var outputDirectory = FullPath.FromPath(args[0]);
             var githubToken = args.Length > 1 ? args[1] : null;
-
-            var repositories = new[]
-            {
-                "dotnet/aspnetcore",
-                "dotnet/roslyn",
-                "dotnet/roslyn-analyzers",
-                "dotnet/runtime",
-            };
-
-            var excludedUsers = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {
-                "dotnet-bot",
-                "dotnet-maestro-bot",
-                "dotnet-maestro[bot]",
-            };
-
-            var excludedLabels = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {
-                "Type: Dependency Update :arrow_up_small:",
-            };
-
-
             var client = githubToken != null ? new GitHubClient(new ProductHeaderValue("issue-to-rss"), new InMemoryCredentialStore(new Credentials(githubToken)))
                                              : new GitHubClient(new ProductHeaderValue("issue-to-rss"));
 
-            await repositories.ParallelForEachAsync(degreeOfParallelism: 16, async repository =>
+            var feeds = new List<FeedData>();
+            await Configuration.Repositories.ParallelForEachAsync(degreeOfParallelism: 16, async repository =>
             {
                 Console.WriteLine("Generating feed for " + repository);
 
                 // Get issues
-                var items = new List<SyndicationItem>(200);
+                var issuesItems = new List<SyndicationItem>(200);
+                var prItems = new List<SyndicationItem>(200);
                 var parts = repository.Split('/');
                 var repositoryOwner = parts[0];
                 var repositoryName = parts[1];
 
                 foreach (var issue in await GetIssuesForRepository(client, repositoryOwner, repositoryName))
                 {
-                    if (excludedUsers.Contains(issue.User.Login))
+                    if (Configuration.ExcludedUsers.Contains(issue.User.Login))
                         continue;
 
-                    if (issue.Labels.Any(label => excludedLabels.Contains(label.Name)))
+                    if (issue.Labels.Any(label => Configuration.ExcludedLabels.Contains(label.Name)))
                         continue;
 
                     var isPullRequest = issue.PullRequest != null;
                     var title = (isPullRequest ? "PR: " : "") + issue.Title;
 
-                    items.Add(new SyndicationItem(title, content: issue.Body, new Uri(issue.HtmlUrl), issue.HtmlUrl, issue.CreatedAt)
+                    var item = new SyndicationItem(title, content: issue.Body, new Uri(issue.HtmlUrl), issue.HtmlUrl, issue.CreatedAt)
                     {
                         Authors =
                         {
                             new SyndicationPerson(issue.User.Email, issue.User.Login, issue.User.HtmlUrl),
                         }
-                    });
+                    };
+
+                    if (isPullRequest)
+                    {
+                        prItems.Add(item);
+                    }
+                    else
+                    {
+                        issuesItems.Add(item);
+                    }
                 }
 
-                // Generate feed
-                var feed = new SyndicationFeed
+                // Generate feeds
                 {
-                    Title = new TextSyndicationContent(repository + " issues"),
-                    Description = new TextSyndicationContent($"Issues from https://github.com/{repository}, generated by https://github.com/meziantou/IssuesToRss"),
-                    Items = items,
-                    TimeToLive = TimeSpan.FromHours(1),
-                    Authors = { new SyndicationPerson("meziantousite@outlook.com", "Gérald Barré", "https://www.meziantou.net/") },
-                };
+                    var issuesFeed = new SyndicationFeed
+                    {
+                        Title = new TextSyndicationContent(repository + " Issues"),
+                        Description = new TextSyndicationContent($"Issues from https://github.com/{repository}, generated by {Configuration.GitHubRepositoryUrl}"),
+                        Items = issuesItems,
+                        TimeToLive = TimeSpan.FromHours(1),
+                        Authors = { new SyndicationPerson("meziantousite@outlook.com", "Gérald Barré", "https://www.meziantou.net/") },
+                    };
 
-                var feedPath = FullPath.Combine(outputDirectory, repositoryOwner, repositoryName + ".rss");
+                    lock (feeds)
+                    {
+                        feeds.Add(new FeedData { Feed = issuesFeed, OutputRelativePath = $"{repository}.issues.rss" });
+                    }
+                }
+
+                {
+                    var prFeed = new SyndicationFeed
+                    {
+                        Title = new TextSyndicationContent(repository + " Pull Requests"),
+                        Description = new TextSyndicationContent($"Pull Requests from https://github.com/{repository}, generated by {Configuration.GitHubRepositoryUrl}"),
+                        Items = prItems,
+                        TimeToLive = TimeSpan.FromHours(1),
+                        Authors = { new SyndicationPerson("meziantousite@outlook.com", "Gérald Barré", "https://www.meziantou.net/") },
+                    };
+
+                    lock (feeds)
+                    {
+                        feeds.Add(new FeedData { Feed = prFeed, OutputRelativePath = $"{repository}.pr.rss" });
+                    }
+                }
+            });
+
+            feeds.Sort((a, b) => a.OutputRelativePath.CompareTo(b.OutputRelativePath));
+
+            // Write feeds
+            foreach (var feedData in feeds)
+            {
+                var feedPath = FullPath.Combine(outputDirectory, feedData.OutputRelativePath);
                 Directory.CreateDirectory(feedPath.Parent);
                 using var stream = File.OpenWrite(feedPath);
-                using var xmlWriter = XmlWriter.Create(stream);
-                var rssFormatter = new Rss20FeedFormatter(feed, serializeExtensionsAsAtom: false);
+                using var xmlWriter = XmlWriter.Create(stream, new XmlWriterSettings() { Indent = true });
+                var rssFormatter = new Rss20FeedFormatter(feedData.Feed, serializeExtensionsAsAtom: false);
                 rssFormatter.WriteTo(xmlWriter);
-                xmlWriter.Flush();
-            });
+            }
+
+            // index.opml
+            {
+                Console.WriteLine("Generating opml");
+                var indexPath = FullPath.Combine(outputDirectory, "feeds.opml");
+                Directory.CreateDirectory(indexPath.Parent);
+                var document = new XDocument(
+                    new XElement("opml", new XAttribute("version", "1.0"),
+                        new XElement("head",
+                            new XElement("title", "GitHub issues feeds")),
+                        new XElement("body", feeds.Select(GetOutline).ToArray())));
+
+                using var stream = File.OpenWrite(indexPath);
+                using var xmlWriter = XmlWriter.Create(stream, new XmlWriterSettings() { Indent = true });
+                document.WriteTo(xmlWriter);
+
+                static IEnumerable<XElement> GetOutline(FeedData data)
+                {
+                    yield return new XElement("outline",
+                        new XAttribute("type", "rss"),
+                        new XAttribute("text", data.Feed.Title),
+                        new XAttribute("title", data.Feed.Title),
+                        new XAttribute("xmlUrl", Configuration.RootUrl + data.OutputRelativePath));
+                }
+            }
 
             // index.html
             {
@@ -99,17 +179,25 @@ namespace IssuesToRss
                 Directory.CreateDirectory(indexPath.Parent);
 
                 var sb = new StringBuilder();
-                sb.Append("<ul>");
-                foreach (var repository in repositories)
+                foreach (var data in feeds)
                 {
                     sb.Append("<li>");
-                    sb.Append($"<a href='{repository}.rss'>{repository}</a>");
+                    sb.Append($"<a href='{data.OutputRelativePath}'>{HtmlEncoder.Default.Encode(data.Feed.Title.Text)}</a>");
                     sb.Append("</li>");
                 }
-                sb.Append("</ul>");
-                sb.Append("<p>Generated by <a href='https://github.com/meziantou/IssuesToRss'>https://github.com/meziantou/IssuesToRss</a></p>");
-                File.WriteAllText(indexPath, sb.ToString());
+
+                var template = GetTemplate("templates/index.html");
+                template = template.Replace("{Feeds}", sb.ToString());
+
+                File.WriteAllText(indexPath, template);
             }
+        }
+
+        private static string GetTemplate(string name)
+        {
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("IssuesToRss." + name.Replace('/', '.'));
+            using var sr = new StreamReader(stream);
+            return sr.ReadToEnd();
         }
 
         private static async Task<IReadOnlyCollection<Issue>> GetIssuesForRepository(GitHubClient client, string owner, string name)
@@ -128,6 +216,12 @@ namespace IssuesToRss
                 PageCount = 2,
             };
             return await client.Issue.GetAllForRepository(owner, name, request, options);
+        }
+
+        private sealed class FeedData
+        {
+            public string OutputRelativePath { get; set; }
+            public SyndicationFeed Feed { get; set; }
         }
     }
 }
